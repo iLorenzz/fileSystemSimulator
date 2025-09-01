cb93ec5bb1ba068633e6c3d1dbf71eb6b825c53d
iLorenzz <ldutra0912@gmail.com>
2025-09-01 10:11:48 -0300

    feat: implement File object with a name and the content element

---
+++ fileSystemCore/src/core/File.java
@@ -0,0 +1,11 @@
+package core;
+
+public class File {
+    private String fileName;
+    private String content;
+
+    public File(String fileName, String content){
+        this.fileName = fileName;
+        this.content = content;
+    }
+}
