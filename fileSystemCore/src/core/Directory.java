5d12e090a91f419852e0240eb44373d906328707
iLorenzz <ldutra0912@gmail.com>
2025-09-01 10:02:05 -0300

    feat: implement Directory object with a name and a list of others directories

--- fileSystemCore/src/core/Directory.java
+++ fileSystemCore/src/core/Directory.java
@@ -1,4 +1,22 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Directory {
+    private String name;
+    private List<Directory> sonDirectories;
+
+    public Directory(String name){
+        this.name = name;
+        sonDirectories = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 }
